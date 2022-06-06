9d119457af2261732383f62243478cb02b214883
aayush-meshram <aayushmeshram007@gmail.com>
2022-06-06 14:38:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The Daily News."),
+        centerTitle: true,
+      ),
+      body: Text("Home page."),
+    );
+  }
+}
